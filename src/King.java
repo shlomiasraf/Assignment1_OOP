62875c0a2fa939aab8bd20a5d71d4d1200a873ec
Shlomi Asraf <148898079+shlomiasraf@users.noreply.github.com>
2024-01-28 16:28:41 +0200

    Add files via upload

---
+++ src/King.java
@@ -0,0 +1,7 @@
+
+public class King extends ConcretePiece{
+    public King(Player owner, String type, String name, String locations)
+    {
+        super(owner, type, name,locations);
+    }
+}
---
+++ src/Pawn.java
@@ -0,0 +1,33 @@
+
+public class Pawn extends ConcretePiece{
+    int kills = 0;
+    public Pawn(Player owner, String type, String name, String locations)
+    {
+        super(owner, type, name,locations);
+    }
+
+    /**
+     *
+     * @return the number of pieces this piece killed.
+     */
+    public int getKills()
+    {
+        return this.kills;
+    }
+
+    /**
+     * @brief add 1 kill to the number of kills this piece killed.
+     */
+    public void addKill()
+    {
+        this.kills++;
+    }
+
+    /**
+     * @brief reduce 1 kill to the number of kills this piece killed.
+     */
+    public void reduceKill()
+    {
+        this.kills--;
+    }
+}
