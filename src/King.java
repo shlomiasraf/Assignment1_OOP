54befb3133ac5981ceb1891bce611ec403612fcb
Shlomi Asraf <148898079+shlomiasraf@users.noreply.github.com>
2024-01-28 16:27:12 +0200

    Delete src directory

--- src/King.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class King extends ConcretePiece{
-    public King(Player owner, String type, String name, String locations)
-    {
-        super(owner, type, name,locations);
-    }
-}
