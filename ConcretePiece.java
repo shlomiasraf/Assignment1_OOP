42fa4d1623aa099b82d710ebdd4bb1f8cfbb3d2e
Shlomi Asraf <148898079+shlomiasraf@users.noreply.github.com>
2024-01-28 19:28:32 +0200

    Add files via upload

---
+++ ConcretePiece.java
@@ -0,0 +1,107 @@
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class ConcretePiece implements Piece {
+    Player owner;
+    String type;
+    String name;
+    ArrayList<String> locations = new ArrayList<>();
+    int square = 0;
+    public ConcretePiece(Player owner, String type,String name,String locations)
+    {
+        this.owner = owner;
+        this.type = type;
+        this.name = name;
+        this.locations.add(locations);
+    }
+
+    /**
+     *
+     * @return the owner of this concrete piece.
+     */
+    @Override
+    public Player getOwner() {
+        return this.owner;
+    }
+
+    /**
+     *
+     * @return the type of this concrete piece.
+     */
+    @Override
+    public String getType()
+    {
+        return this.type;
+    }
+
+    /**
+     * @brief checks if the objects are equals.
+     * @param obj the object we want to compare.
+     * @return true if the objects are equals.
+     */
+    @Override
+    public boolean equals(Object obj)
+    {
+        ConcretePiece piece = (ConcretePiece) obj;
+        if(piece.getOwner() == owner && piece.getType().equals(type))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @brief calculate the cumulative distance this concrete piece has done.
+     */
+    public void setSquare()
+    {
+        if(getLocations().size() > 1)
+        {
+            for (int i = 1; i < getLocations().size(); i++)
+            {
+                int x1 = Integer.parseInt(getLocations().get(i).substring(1,(getLocations().get(i).indexOf(","))));
+                int x2 = Integer.parseInt(getLocations().get(i-1).substring(1,(getLocations().get(i-1).indexOf(","))));
+                int y1 = Integer.parseInt(getLocations().get(i).substring((getLocations().get(i).indexOf(",")+2),getLocations().get(i).length()-1));
+                int y2 = Integer.parseInt(getLocations().get(i-1).substring((getLocations().get(i-1).indexOf(",")+2),getLocations().get(i-1).length()-1));
+                this.square += Math.hypot(x1-x2, y1-y2);
+            }
+        }
+    }
+
+    /**
+     * @brief removes the last position this concrete piece stepped on.
+     */
+    public void removeLastLoc()
+    {
+        this.locations.remove(this.locations.size()-1);
+    }
+
+    /**
+     *
+     * @return the positions this concrete piece stepped on.
+     */
+    public ArrayList<String> getLocations()
+    {
+        return this.locations;
+    }
+
+    /**
+     *
+     * @return the cumulative distance this concrete piece has done.
+     */
+    public int getSquare()
+    {
+        return this.square;
+    }
+
+    /**
+     *
+     * @return the name of this concrete piece.
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+}
